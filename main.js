56ef45560d0c98a1f3264c53c72de73e8aec0dcb
SkyllarRisen <20288996+SkyllarRisen@users.noreply.github.com>
2023-10-21 00:36:35 +0200

    added functionality for displaying/hiding images

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
+
+
+
+
+
+function showImage() {
+    var imageElement = document.getElementById('myImage');
+    imageElement.classList.remove('hidden');
+}
+
+
 function testOutput()
 {
     console.log("testmebaby");
 }
+
+function checkWebsiteStatus(url, callback) 
+{
+    fetch(url, {
+        method: 'GET',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    })
+    .catch(() => {
+        console.log("error.");
+        callback(false);
+    });
+}
+
+// Example usage:
+const websiteUrl = 'https://pfsense.arnold-of.de:10443';
+checkWebsiteStatus(websiteUrl, (isOnline) => {  if (isOnline) {console.log('Website is online.');} else {console.log('Website is offline.');}});
+console.log("imhere");
