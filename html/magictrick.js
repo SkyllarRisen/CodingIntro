f1c8925548f67c798ad28e21b95cc674f7628ee1
Philipp Arnold <20288996+SkyllarRisen@users.noreply.github.com>
2023-10-26 14:11:59 +0200

    Added input form and display result in container

--- html/magictrick.js
+++ html/magictrick.js
@@ -4,13 +4,24 @@ function calculateBMI(kg, height)
     return kg / (height*height);
 }
 
-
-
-
 function getBMI()
 {
-let kg = parseFloat(prompt("Geben sie ihr Gewicht in kg ein: "));
-let height = parseFloat(prompt("Geben Sie ihre Körpergröße in Metern ein: "));
 
-alert("Wenn sie "+ height +" m groß sind und "+ kg +" kg wiegen, dann lautet ihr BMI: " + calculateBMI(50, 1.60) +" (auf ganze Zahl gerundet: "+ Math.round(calculateBMI(50, 1.60))+").");
+let kg = parseFloat(document.getElementById("weightInput").value);
+let height = parseFloat(document.getElementById("heightInput").value);
+
+if(isNaN(kg)|| isNaN(height))
+{
+    alert("Please enter valid numbers.");
+    return ;   
+}
+
+document.getElementById("bmiResult").innerHTML = "Wenn sie "+ height +" m groß sind und "+ kg +" kg wiegen, <br> dann lautet ihr BMI: <br><br>" + calculateBMI(kg, height) 
+    +" (auf ganze Zahl gerundet: "+ Math.round(calculateBMI(kg, height))+").";
+
+}
+
+function maxNumber(a,b,c)
+{
+    return Math.max(a, Math.max(b,c));
 }
