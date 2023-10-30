f4026c22dc0e7a60e2958a4f87431b6d6d23f290
Philipp Arnold <20288996+SkyllarRisen@users.noreply.github.com>
2023-10-30 09:51:02 +0100

    fixed youtubeplayer offset

--- html/aboutme.js
+++ html/aboutme.js
@@ -1,13 +1,39 @@
 
 
+function initTable()
+{
+    const dataTable = document.getElementById("dataTable").getElementsByTagName('tbody')[0];
+
+
+    const fixedData = [
+        {
+            Name: "Philipp Arnold", 
+            Alter: "29",
+            Stadt: "Offenbach am Main"
+        }
+    ];
+
+    fixedData.forEach(function (datensatz) {
+        const row = dataTable.insertRow();
+        const nameCell = row.insertCell(0);
+        const ageCell = row.insertCell(1);
+        const cityCell = row.insertCell(2);
+    
+        nameCell.innerHTML = datensatz.Name;
+        ageCell.innerHTML = datensatz.Alter;
+        cityCell.innerHTML = datensatz.Stadt;
+    });
+
+}
+
+initTable();
 
 function fillTable()
 {
 
     const dataTable = document.getElementById("dataTable").getElementsByTagName('tbody')[0];
 
-
-    const daten = [
+    const data = [
         {
         Name: document.getElementById("Name").value, 
         Alter: document.getElementById("Alter").value,
@@ -15,10 +41,7 @@ function fillTable()
         }
     ];
 
-
-
-
-    daten.forEach(function (datensatz) {
+    data.forEach(function (datensatz) {
         const row = dataTable.insertRow();
         const nameCell = row.insertCell(0);
         const ageCell = row.insertCell(1);
