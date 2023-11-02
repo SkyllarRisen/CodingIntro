2b948fbc4640cbe3accb5453354f6c968d13bbe6
SkyllarRisen <20288996+SkyllarRisen@users.noreply.github.com>
2023-11-02 17:47:58 +0100

    logging

--- services/auth.js
+++ services/auth.js
@@ -53,6 +53,7 @@ app.post('/login', (req, res) => {
 // Register route
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
+  console.log(req);
   console.log(req.body);
   console.log(username + " : " + password);  
   bcrypt.hash(password, 10, (err, hash) => {
